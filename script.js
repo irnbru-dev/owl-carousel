6c84aaf4aed135a7c3adaee0b7e5f0c6fadcf4a4
Konovalova <darina.konovalova@it-grad.ru>
2018-01-15 17:43:33 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 6,
+        margin: 15,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 5
+            },
+            1200: {
+                items: 6
+            }
+        }
+    })
+});
